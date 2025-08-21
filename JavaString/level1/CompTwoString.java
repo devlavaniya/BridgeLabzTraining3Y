8aa03b6fcff6fab8b856720c2b82da719aa0d4b4
devlavaniya <devlavaniya18@gmail.com>
2025-08-21 16:05:50 +0530

    Added example

---
+++ JavaString/level1/CompTwoString.java
@@ -0,0 +1,11 @@
+package BridgeLabzTraining3Y.JavaString.level1;
+
+import java.util.Scanner;
+
+public class CompTwoString {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+    }
+ 
+    
+}
